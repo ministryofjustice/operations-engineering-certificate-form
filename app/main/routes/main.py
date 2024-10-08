04cba58c506f95b96b2933a7a53762db81bbde5e
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-10-08 16:04:19 +0100

    :recycle: Corrected validation for requestor email and requestor name.

--- app/main/routes/main.py
+++ app/main/routes/main.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
+
+from app.main.validators.index import validate_certificate_form
 
 main = Blueprint("main", __name__)
 
@@ -10,28 +12,18 @@ def index():
 
 @main.route('/submit-a-csr', methods=['GET', 'POST'])
 def submit_csr():
-    form_data = {}
-    errors = {}
 
-    if request.method == 'POST':
-        requestor_name = request.form.get('requestor_name')
-        requestor_email = request.form.get('requestor_email')
-        csr_input_field = request.form.get('csr_input_field')
-
-        if not requestor_name:
-            errors['requestor_name'] = "Requestor name is required."
-        if not requestor_email:
-            errors['requestor_email'] = "Requestor email is required."
-        if not csr_input_field or len(csr_input_field) <= 20:
-            errors['csr_input_field'] = "CSR must be more than 20 characters."
-
-        form_data = {
-            'requestor_name': requestor_name,
-            'requestor_email': requestor_email,
-            'csr_input_field': csr_input_field
-        }
+    if request.method == "POST":
+        form_data = request.form.to_dict()
+        errors = validate_certificate_form(form_data)
+        if errors:
+            for field, error_message in errors.items():
+                flash((field, error_message), "form_error")
+            return render_template(
+                "pages/submit_csr_page.html", form_data=form_data, errors=errors
+            )
 
         if not errors:
             return render_template("pages/confirmation.html", issue_number=2)
 
-    return render_template('pages/submit_csr_page.html', form_data=form_data, errors=errors)
+    return render_template('pages/submit_csr_page.html')
