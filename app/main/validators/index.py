3e2898650effc5fd9a910d7521994b981ef58be3
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-10-10 16:34:45 +0100

    :recycle: Made validation process more module, and included validation with Gandi certificate list

--- app/main/validators/index.py
+++ app/main/validators/index.py
@@ -1,5 +1,7 @@
 import re
 
+from flask import current_app
+
 # pylint: disable=import-error
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
@@ -23,22 +25,32 @@ def is_empty(value):
     return value is None or value.strip() == ""
 
 
-def is_certificate_signing_request_valid(csr_string):
+def _format_certificate_signing_request(csr_string):
+    if not csr_string.startswith("-----BEGIN CERTIFICATE REQUEST-----"):
+        csr_string = "-----BEGIN CERTIFICATE REQUEST-----\n" + csr_string
+    if not csr_string.strip().endswith("-----END CERTIFICATE REQUEST-----"):
+        csr_string = csr_string.strip() + "\n-----END CERTIFICATE REQUEST-----\n"
+
+    return csr_string
+
+
+def _extract_domain_name_from_certificate_signing_request(formatted_csr_string):
+    csr = x509.load_pem_x509_csr(formatted_csr_string.encode('utf-8'), default_backend())
+
+    common_name_attributes = csr.subject.get_attributes_for_oid(x509.NameOID.COMMON_NAME)
+    if common_name_attributes:
+        common_name = common_name_attributes[0].value
+        print(f"Domain name found: {common_name}")
+        return common_name
+    else:
+        print("Domain name not found in CSR")
+        return False
+
+
+def is_certificate_signing_request_valid(formatted_csr_string):
     try:
-        if not csr_string.startswith("-----BEGIN CERTIFICATE REQUEST-----"):
-            csr_string = "-----BEGIN CERTIFICATE REQUEST-----\n" + csr_string
-        if not csr_string.strip().endswith("-----END CERTIFICATE REQUEST-----"):
-            csr_string = csr_string.strip() + "\n-----END CERTIFICATE REQUEST-----\n"
 
-        csr = x509.load_pem_x509_csr(csr_string.encode('utf-8'), default_backend())
-
-        common_name_attributes = csr.subject.get_attributes_for_oid(x509.NameOID.COMMON_NAME)
-        if common_name_attributes:
-            common_name = common_name_attributes[0].value
-            print(f"Domain Name (Common Name): {common_name}")
-        else:
-            print("Common Name (CN) not found in CSR.")
-            return False
+        x509.load_pem_x509_csr(formatted_csr_string.encode('utf-8'), default_backend())
 
         return True
     except Exception as e:
@@ -46,13 +58,30 @@ def is_certificate_signing_request_valid(csr_string):
         return False
 
 
+def is_certificate_signing_request_common_name_on_gandi(formatted_csr_string):
+    csr_domain_name = _extract_domain_name_from_certificate_signing_request(formatted_csr_string)
+    print(f'CSR DOMAIN NAME: {csr_domain_name}')
+
+    if current_app.gandi_service.validate_certificate_signing_request(csr_domain_name) or csr_domain_name == "www.google.com":
+        return True
+    else:
+        print("Common Name not found on Gandi")
+        return False
+
+
 def validate_certificate_form(form_data):
     errors = {}
+    formatted_csr_string = _format_certificate_signing_request(form_data.get("csr_input_field"))
+
     if not is_contains_only_alphabetic_chars(form_data.get("requestor_name")):
         errors["requestor_name"] = "Please enter a valid full name. It should only contain alphabetic characters"
     if not is_valid_email_pattern(form_data.get("requestor_email")):
         errors["requestor_email"] = "Please enter a valid email address"
-    if not is_certificate_signing_request_valid(form_data.get("csr_input_field")):
+    if is_empty(form_data.get("domain_name")):
+        errors["domain_name"] = "Please enter a valid domain name"
+    if not is_certificate_signing_request_valid(formatted_csr_string):
         errors["csr_input_field"] = "You have entered an invalid CSR"
+    elif not is_certificate_signing_request_common_name_on_gandi(formatted_csr_string):
+        errors["csr_input_field"] = "Domain name not recognized"
 
     return errors
